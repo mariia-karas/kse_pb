ac38b47ebf62fb302b7129064e724f4c9ba2bcc9
mariia-karas <mkaras@kse.org.ua>
2025-05-30 23:40:34 +0300

    Rename folder 'assignment_1' to 'assignment_2'

---
+++ CS50/return_value.py
@@ -0,0 +1,8 @@
+def greet(input):
+  if "hello" in input :
+    return "hello, nigga"
+  else:
+    return "wtf"
+
+greet("hello bro")
+ 
---
+++ lecture_2/tryouts.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+if age <0:
+    print("Impossible!!")
+elif age <= 5:
+    print("Baby")
+elif age <=12:
+    print("Pupil")
+else:
+    print("You can take meds")
